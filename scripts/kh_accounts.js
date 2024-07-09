b5f3bce3b96118a6f5e50f0d7e0505a42c6b7746
khester1 <kylehester@outlook.com>
2024-07-08 20:40:06 -0700

    feat: Add openDialog function to Xrm.Scripts.Account.Ribbon

---
+++ scripts/kh_accounts.js
@@ -0,0 +1,52 @@
+if (typeof (Xrm) == "undefined") {
+    var Xrm = {
+        __namespace: true
+    };
+}
+if (typeof (Xrm.Scripts) == "undefined") {
+    Xrm.Scripts = {
+        __namespace: true
+    };
+}
+
+if (typeof (Xrm.Scripts.Account) == "undefined") {
+    Xrm.Scripts.Account = {
+        __namespace: true
+    };
+}
+
+Xrm.Scripts.Account.Ribbon = {
+    OpenDialog: function (executionContext) {
+        var data = {
+            text: "test",
+            date: new Date(),
+        };
+
+        var dialogParameters = {
+            pageType: "webresource",
+            webresourceName: "kh_/DialogExample/index.html",
+            data: JSON.stringify(data)
+        };
+        
+        var navigationOptions = {
+            target: 2, 
+            width: 400,
+            height: 410,
+            position: 1,
+            title: "Example Dialog",
+        };
+
+        Xrm.Navigation.navigateTo(dialogParameters, navigationOptions).then(
+            function (returnValue) {
+                console.log(returnValue);
+                // Add your processing logic here
+            },
+            function (e) {
+                Xrm.Navigation.openErrorDialog(e);
+            }
+        ); 
+    }
+};
+
+
+//Xrm.Scripts.Account.Ribbon.OpenDialog(executionContext);
